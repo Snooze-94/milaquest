2afae55df97c21c5eb20cb7686a0bd3a4c641010
alex <alejandroaamaya@gmail.com>
2018-05-25 19:10:58 -0300

    lets go boys

---
+++ Assets/Scripts/Player/GroundCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollider : MonoBehaviour {
+
+	public static bool grounded = false;
+	
+	void OnTriggerStay(Collider other)	
+	{
+		grounded = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		grounded = false;
+	}
+
+}
